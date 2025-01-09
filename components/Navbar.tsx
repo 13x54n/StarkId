68abb0947de25a4b42508ee5e451e866deb49f69
root <root@DESKTOP-3G364I3.localdomain>
2025-01-09 08:39:51 -0500

    Update on the page.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
   return (
     <div className='p-3 py-2 flex items-center justify-between mx-[5vw]'>
       <Image className='w-[120px] object-contain' src={logo} alt="StarkId Logo" />
-      <button className='bg-black text-white text-sm px-2 py-1'>Launch App</button>
+      <button className='bg-black text-white text-sm px-2 py-1.5 rounded-lg flex items-center gap-1 hover:bg-gray-800 transition-all ease-in-out'><i className="ri-fingerprint-line"></i> Launch App</button>
     </div>
   );
 }
